87a084bd7d72d5819e32b4f014019d6dca42f705
System Administrator <root@Ramyas-MacBook-Air.local>
2025-10-06 02:00:39 +0530

    2nd for oidc

---
+++ iam.tf
@@ -0,0 +1,38 @@
+# iam.tf
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-deploy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:Theramya10/portfolio_infra:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "github_policy_attach" {
+  name       = "github-policy-attach"
+  roles      = [aws_iam_role.github_actions.name]
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
